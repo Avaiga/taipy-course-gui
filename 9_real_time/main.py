6f01321654ceced09126afcb814fd8cea2644c8d
Zaccheus <48582724+arcanaxion@users.noreply.github.com>
2025-07-04 16:29:23 +0800

    Change real-time data update example (#4)

---
+++ 9_real_time/main.py
@@ -0,0 +1,27 @@
+import datetime as dt
+import time
+from threading import Thread
+
+import taipy.gui.builder as tgb
+from taipy.gui import Gui
+
+
+def update_time(gui: Gui):
+    while True:
+        gui.broadcast_callback(
+            callback=lambda state: state.assign("current_time", dt.datetime.now().strftime("%H:%M:%S"))
+        )
+        time.sleep(1)
+
+
+current_time = "HH:MM:SS"
+
+with tgb.Page() as page:
+    tgb.text("Current time: {current_time}")
+
+gui = Gui(page=page)
+
+t = Thread(target=update_time, args=(gui,))
+t.start()
+
+gui.run(title="Real-time data updates")
