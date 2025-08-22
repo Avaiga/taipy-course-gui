c48c37100ae04158fac66014a4ba6e43bedfb7b0
Zaccheus Sia <arcanaxion@gmail.com>
2025-08-22 09:56:56 +0800

    typo: Accidentally committed run_browser=False

--- 4_charts/main.py
+++ 4_charts/main.py
@@ -112,4 +112,4 @@ with tgb.Page() as page:
         tgb.html("br")
         tgb.table(data="{data}")
 
-Gui(page=page).run(title="Sales", dark_mode=False, run_browser=False)
+Gui(page=page).run(title="Sales", dark_mode=False)
